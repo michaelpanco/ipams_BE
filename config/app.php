c9a2d11b2ac726214b6fe0477cc29c66146a7b7f
Michael Panco <micpanco@gmail.com>
2021-08-13 03:12:22 +0800

    centralized configuration variables

---
+++ config/app.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | IconPractice API
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of your application. This value is used when the
+    | framework needs to place the application's name in a notification or
+    | any other location as required by the application or its packages.
+    |
+    */
+    'name' => env('APP_NAME', 'IP Address Management Solution'),
+    'env' => env('APP_ENV', 'local'),
+    'debug' => env('APP_DEBUG', false),
+    'jwt_secret' => env('JWT_SECRET'),
+
+];
